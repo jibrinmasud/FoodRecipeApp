91cd2688ab92198a3bd06991887322cd98fbd32b
Jibrin Masud <jibrinmasud494@gmail.com>
2023-11-08 15:32:41 +0100

    Creating a food item component to store foods

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch?";
 const API_KEY = "d0950b6136bb4652b63ffc451829a7ed";
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
+
   //useEffect Hooks
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
